f89f006e9d97525604ed856e43601b2e142ee1d8
divyaskills <divya.modi36@gmail.com>
2020-05-25 11:54:18 +0530

    3 files modified

--- src/app/chips-overview-example.ts
+++ src/app/chips-overview-example.ts
@@ -1,21 +1,21 @@
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
 
 /**
  * @title Basic chips
  */
 @Component({
-  selector: 'chips-overview-example',
-  templateUrl: 'chips-overview-example.html',
-  styleUrls: ['chips-overview-example.css'],
+  selector: "chips-overview-example",
+  templateUrl: "chips-overview-example.html",
+  styleUrls: ["chips-overview-example.css"]
 })
 export class ChipsOverviewExample {
-  selectedOption: string = 'All';
+  selectedOption: string = "All";
+  searchString: string = "";
 
   onSelectedItem(type: string): void {
     this.selectedOption = type;
   }
 }
-
 
 /**  Copyright 2019 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
